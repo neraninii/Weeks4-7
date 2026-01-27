c159beb315ce88f0a35cafb0649f4febb059f821
Kinn <neraninii@gmail.com>
2026-01-26 23:09:15 -0500

    button creation

---
+++ Assets/Scripts/UIDemo.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class UIDemo : MonoBehaviour
+{
+    SpriteRenderer sr;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(Keyboard.current.anyKey.wasPressedThisFrame == true)
+        {
+            ChangeColour();
+        }
+    }
+
+    void ChangeColour()
+    {
+        sr.color = Random.ColorHSV();
+    }
+}
