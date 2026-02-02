e9106f1878d767af82f0bcfd52a7fbfdd7dbafa5
Kinn <neraninii@gmail.com>
2026-02-02 10:08:40 -0500

    Toggled a game object with a button

---
+++ Assets/Scripts/Toggle.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Toggle : MonoBehaviour
+{
+    public void ToggleBird()
+    {
+        Debug.Log("Toggle the bird please!");
+        //gameObject.SetActive(false); //SetActive sets the value
+        //gameObject.activeInHierarchy //this tells us if the box is ticked 
+
+        //if the bird is active, call SetActive and pass false
+        //if(gameObject.activeInHierarchy == true)
+        //{
+       //     gameObject.SetActive(false);
+        //}
+        //else if(gameObject.activeInHierarchy == false)
+        //{
+        //    // (otherwise) if the bird is not active, call SetActive and pass true 
+            gameObject.SetActive(true);
+        //}
+
+        gameObject.SetActive(!gameObject.activeInHierarchy);
+        
+    }
+}
