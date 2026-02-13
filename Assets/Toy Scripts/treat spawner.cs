3b40888132221ba764b5844d0dc727909159f166
Kinn <neraninii@gmail.com>
2026-02-12 23:02:32 -0500

    treat mechanics

--- Assets/Toy Scripts/treat spawner.cs
+++ Assets/Toy Scripts/treat spawner.cs
@@ -2,15 +2,36 @@ using UnityEngine;
 
 public class treatspawner : MonoBehaviour
 {
+
+    public GameObject applePrefab;
+    public GameObject carrotPrefab;
+
+    SpriteRenderer sr; 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        sr = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void AppleSpawn()
+    {
+        Instantiate(applePrefab, transform.position, transform.rotation);
+        
+    }
+
+    public void CarrotSpawn()
+    {
+        Instantiate(carrotPrefab, transform.position, transform.rotation);
+    }
+
+    public void BigTreat(float scale)
+    {
+        transform.localScale = Vector3.one * scale;
     }
 }
